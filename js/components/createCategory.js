3ac3c0f3113c712f7eda4cd5d2a1d6d07ad9694b
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2023-07-21 16:25:13 +0200

    added dec of num

--- js/components/createCategory.js
+++ js/components/createCategory.js
@@ -1,4 +1,5 @@
 import { createElement } from "../helper/createElement.js";
+import { delOfNum } from "../helper/delOfNum.js";
 
 export const createCategory = (app) => {
   const category = createElement("section", {
@@ -18,9 +19,36 @@ export const createCategory = (app) => {
   const createCategoryCard = (data) => {
     const item = createElement("li", {
       className: "category__item",
-      textContent: data.title,
     });
     item.dataset.id = data.id;
+
+    const btnCard = createElement("button", {
+      className: "category__card",
+    });
+
+    const titleText = createElement("span", {
+      className: "category__title",
+      textContent: data.title,
+    });
+
+    const countPairs = createElement("span", {
+      className: "category__pairs",
+      textContent: delOfNum(data.length, ["пара", "пары", "пар"]),
+    });
+
+    btnCard.append(titleText, countPairs);
+
+    const btnEdit = createElement("button", {
+      className: "category__btn category__edit",
+      ariaLabel: "редактировать",
+    });
+
+    const btnDel = createElement("button", {
+      className: "category__btn category__del",
+      ariaLabel: "удалить",
+    });
+
+    item.append(btnCard, btnEdit, btnDel);
 
     return item;
   };
